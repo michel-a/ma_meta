097f1e79bda89053c30eb7c6538900858d476948
Michel Alves <malvesdesign@hotmail.com>
2022-07-11 22:02:55 -0300

    Project created

---
+++ backend/src/main/java/com/malves/mameta/MametaApplication.java
@@ -0,0 +1,13 @@
+package com.malves.mameta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MametaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MametaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/malves/mameta/MametaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malves.mameta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MametaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
